f61a64e234194d06d801cb838b886b9c027e8571
O-sulloc <bujjaf@gmail.com>
2022-10-06 09:52:48 +0900

    main init

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("new repository");
+        System.out.println("switch default branch to main");
     }
 }
