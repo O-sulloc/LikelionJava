cffb2ccb9453ee35fa52959f0bd4ad7f17ed4e9b
O-sulloc <bujjaf@gmail.com>
2022-10-06 15:16:04 +0900

    dir print

--- src/week3/day3/fileStudy/FileExercise.java
+++ src/week3/day3/fileStudy/FileExercise.java
@@ -5,10 +5,19 @@ import java.io.FileNotFoundException;
 
 public class FileExercise {
     public static void main(String[] args) throws FileNotFoundException {
-        File dir = new File("./"); // [./] -> 현재 디렉토리에서 파일 찾아
-        File files[] = dir.listFiles(); //찾은 파일들을 files배열에 담아
+        File dir = new File("./"); // [./] -> root 디렉토리에서 파일 찾아
+        File files[] = dir.listFiles(); //찾은 파일들을 files 배열에 담아
         for (File file : files) {
             System.out.println(file); //반복문으로 출력해
+                   /* ./out
+                    ./README.md
+                    ./.gitignore
+                    ./.git
+                    ./LikelionJava.iml
+                    ./a_file.txt
+                    ./.idea
+                    ./src */
         }
+
     }
 }
