46a8775319cf6a9fdf7bfe8bd2d5061cd5516af0
O-sulloc <bujjaf@gmail.com>
2022-10-06 14:34:20 +0900

    FileRead Study

---
+++ src/week3/day3/fileStudy/ReadAFile.java
@@ -0,0 +1,12 @@
+package week3.day3.fileStudy;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadAFile {
+    public static void main(String[] args) throws IOException {
+        FileReader reader = new FileReader("./a_file.txt");
+        char c = (char) reader.read();
+        System.out.println(c);
+    }
+}
---
+++ src/week3/day3/fileStudy/ReadFile.java
@@ -0,0 +1,19 @@
+package week3.day3.fileStudy;
+
+public class ReadFile {
+    private String fileName;
+
+    public ReadFile(String fileName){
+        this.fileName = fileName;
+    }
+
+    char readAByte(String fileName){
+        return ' ';
+    }
+
+//    public static void main(String[] args) {
+//        ReadFile readFile = new ReadFile("./a_file.txt"){
+//
+//        }
+//    }
+}
