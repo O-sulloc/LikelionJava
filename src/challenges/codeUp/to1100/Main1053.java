f594abee8a54cf5ff9af0518b490692f9c509f46
O-sulloc <bujjaf@gmail.com>
2022-10-13 00:04:50 +0900

    codeup 1051~1060

---
+++ src/challenges/codeUp/to1100/Main1053.java
@@ -0,0 +1,13 @@
+package challenges.codeUp.to1100;
+
+import java.util.Scanner;
+
+public class Main1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.println(num == 1 ? 0:1);
+    }
+}
---
+++ src/challenges/codeUp/to1100/Main1054.java
@@ -0,0 +1,19 @@
+package challenges.codeUp.to1100;
+
+import java.util.Scanner;
+
+public class Main1054 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a == 1 && b == 1){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+
+    }
+}
