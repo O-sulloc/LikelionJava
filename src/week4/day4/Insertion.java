1e6dbeb7ef564b83ef83c75a4a558c5caa1376c3
O-sulloc <bujjaf@gmail.com>
2022-10-14 10:15:50 +0900

    Insertion sort swap

--- src/week4/day4/Insertion.java
+++ src/week4/day4/Insertion.java
@@ -3,16 +3,18 @@ package week4.day4;
 import java.util.Arrays;
 
 public class Insertion {
-    public int[] sort(int[] arr) {
+    public int[] sort(int[] arr, int i) {
         //매개 변수로 arr배열을 받고 리턴할 것임.
         //arr을 오름차순으로 정렬하는 메서드를 만들 것.
 
-        int i = 1;
+        //int i = 1; //2번째 인덱스부터 조회한다는거
         int temp = 0;
-        if (arr[i] < arr[i - 1]) { //5<8 작으면 swap
-            temp = arr[i - 1];
-            arr[i - 1] = arr[i];
-            arr[i] = temp;
+
+        int j = i;
+        if (arr[j] < arr[j - 1]) { //5<8 작으면 swap
+            temp = arr[j - 1]; //temp에 8 넣고
+            arr[j - 1] = arr[j]; //첫번째 인덱스에 5 넣고
+            arr[j] = temp; //두번째 인덱스에 8 넣고
         }
 
 
@@ -20,10 +22,13 @@ public class Insertion {
     }
 
     public static void main(String[] args) {
-        //5ㅇㅔ서 출발
-        int[] arr = {8, 5, 6, 2, 4};
+        //숫자 5부터 출발
+        //int[] arr = {8, 5, 6, 2, 4};
+
         Insertion is = new Insertion();
-        int[] result = is.sort(arr);
+
+        int[] arr = {5, 8, 6, 2, 4};
+        int[] result = is.sort(arr, 2);
 
         System.out.println(Arrays.toString(result));
     }
