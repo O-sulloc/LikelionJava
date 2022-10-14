18f0c43c5c9938e3d0c7c7bf2ac71e700702d328
O-sulloc <bujjaf@gmail.com>
2022-10-14 09:47:45 +0900

    Insertion sort swap

--- src/week4/day4/Insertion.java
+++ src/week4/day4/Insertion.java
@@ -3,18 +3,25 @@ package week4.day4;
 import java.util.Arrays;
 
 public class Insertion {
-    public int[] sort(int[] arr){
+    public int[] sort(int[] arr) {
         //매개 변수로 arr배열을 받고 리턴할 것임.
         //arr을 오름차순으로 정렬하는 메서드를 만들 것.
 
-        int i=1;
+        int i = 1;
+        int temp = 0;
+        if (arr[i] < arr[i - 1]) { //5<8 작으면 swap
+            temp = arr[i - 1];
+            arr[i - 1] = arr[i];
+            arr[i] = temp;
+        }
 
-        return new int[]{};
+
+        return arr;
     }
 
     public static void main(String[] args) {
         //5ㅇㅔ서 출발
-        int[] arr = {8,5,6,2,4};
+        int[] arr = {8, 5, 6, 2, 4};
         Insertion is = new Insertion();
         int[] result = is.sort(arr);
 
