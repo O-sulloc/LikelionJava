8d5861a35eed0f27cde374ef8875ac4d78c55d42
O-sulloc <bujjaf@gmail.com>
2022-10-14 09:36:58 +0900

    Insertion sort

---
+++ src/week4/day4/Insertion.java
@@ -0,0 +1,23 @@
+package week4.day4;
+
+import java.util.Arrays;
+
+public class Insertion {
+    public int[] sort(int[] arr){
+        //매개 변수로 arr배열을 받고 리턴할 것임.
+        //arr을 오름차순으로 정렬하는 메서드를 만들 것.
+
+        int i=1;
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        //5ㅇㅔ서 출발
+        int[] arr = {8,5,6,2,4};
+        Insertion is = new Insertion();
+        int[] result = is.sort(arr);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
