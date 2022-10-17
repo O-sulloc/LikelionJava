3dad12f22962492cd3146b14239ba520c6954847
O-sulloc <bujjaf@gmail.com>
2022-10-17 09:09:22 +0900

    star

---
+++ src/week5/day1/Star.java
@@ -0,0 +1,12 @@
+package week5.day1;
+
+public class Star {
+    public static void main(String[] args) {
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+    }
+}
