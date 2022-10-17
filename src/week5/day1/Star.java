e8ef207e01e001d67985d6c4be2ebc6b77e3fc8c
O-sulloc <bujjaf@gmail.com>
2022-10-17 09:26:21 +0900

    [Add] star

--- src/week5/day1/Star.java
+++ src/week5/day1/Star.java
@@ -1,12 +1,20 @@
 package week5.day1;
 
 public class Star {
+    public void triangle(int num) {
+        //매개변수 - 몇 줄 출력할 건지
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.printf("%s", "*");
+            }
+            System.out.printf("\n");
+        }
+    }
+
     public static void main(String[] args) {
+        Star star = new Star();
 
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-
+        star.triangle(4);
     }
 }
