4b4ab21eb2c59184a42c6ad568d606301720b85d
O-sulloc <bujjaf@gmail.com>
2022-10-18 09:19:50 +0900

    [Add] printRectangle

--- src/week5/day2/Square.java
+++ src/week5/day2/Square.java
@@ -1,19 +1,26 @@
 package week5.day2;
 
 public class Square {
-    public void printSquare(int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.printf("*");
+    public void printRectangle(int x, int y) {
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.printf("%s ", "*");
             }
             System.out.println();
         }
-
     }
+
+    public void printSquare(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("* ".repeat(n)); //repeat modern java style
+        }
+    }
+
 
     public static void main(String[] args) {
         Square s = new Square();
 
-        s.printSquare(4);
+        //s.printSquare(4);
+        s.printRectangle(2, 3);
     }
 }
